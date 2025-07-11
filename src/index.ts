f6f74c9ab5ad7bd1ef84c91bcc74dfc6a6c3e2ad
bojitha001 <nawarathnabojitha@gmail.com>
2025-07-11 22:30:08 +0530

    make the folder structre

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config"
 import express from "express";
-import { connectDB } from "../infrastructure/db";
+import { connectDB } from "./config/db";
 import cors from "cors"
-import userRouter from "../api/user";
+import userRouter from "./routes/user";
 
 const app = express();
 app.use(express.json());
