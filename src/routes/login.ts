5b884994e34b990dc526b1d870205a78d2790640
Adithya306 <garukaadithya@gmail.com>
2025-08-08 18:05:59 +0530

    init commit

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,9 +1,23 @@
-import { userLogin } from "../controllers/auther";
+// import { userLogin } from "../controllers/auther";
+
+// const express = require("express");
+
+// const loginRouter = express.Router();
+
+// loginRouter.route("/").post(userLogin);
+
+// export default loginRouter;
+
+import { userLogin, userLogout } from "../controllers/auther";
 
 const express = require("express");
 
 const loginRouter = express.Router();
 
+// Login route
 loginRouter.route("/").post(userLogin);
+
+// Logout route
+loginRouter.route("/logout").post(userLogout);
 
 export default loginRouter;
