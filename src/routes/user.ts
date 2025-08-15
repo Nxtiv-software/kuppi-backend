98abf385b2d4a1178a7eb4a58f30015833069262
Adithya306 <garukaadithya@gmail.com>
2025-08-15 09:26:25 +0530

    update user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,29 @@ import { addUser } from "../controllers/user";
 
 const userRouter = express.Router();
 
+// FOR LOCALHOST TESTING: Skip authentication middleware
+// Comment out this line when deploying
+userRouter.use((req, res, next) => {
+  console.log('Skipping authentication for user routes - localhost testing');
+  
+  // Mock user in request if needed by controllers
+  if (!(req as any).user) {
+    (req as any).user = {
+      _id: 'test-user-123',
+      id: 'test-user-123',
+      name: 'Test User',
+      email: 'test@example.com'
+    };
+  }
+  
+  next();
+});
+
+// AUTHENTICATION: Apply authentication middleware when deploying
+// Uncomment when deploying with authentication
+// import authenticateUser from '../middlewares/auth';
+// userRouter.use(authenticateUser);
+
 userRouter.route("/").post(addUser);
 
 export default userRouter;
